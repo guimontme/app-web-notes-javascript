6fd0d82261730e497ed501248f2512121822e67f
guimont <gspmonteiro@gmail.com>
2020-06-20 14:11:12 -0300

    teste

--- src/_JS/class-note-controller.js
+++ src/_JS/class-note-controller.js
@@ -13,39 +13,42 @@ class noteController{
 
     initialize(){
         this.clickPlusNote();
-
+        
     }
 
     clickPlusNote(){
         this.btn_plus_note.addEventListener("click", ()=> {
-            this.templateNote();
+            if(!this.i) this.i = 1;
+            this.templateNote(this.i);
             this.clickNote();
+            this.i++;
         })
     }
 
-    templateNote() {
+    templateNote(i) {
 
-           this.box_single_note.innerHTML += ("<div class='note'>" +
+           this.box_single_note.innerHTML += ("<div class='note' id='note_"+ i +"'>" +
                                    "<header class='flex'>" +
                                        "<div class='edits'>" +
-                                           "<i class='fas fa-pencil-alt c-orange'></i>" +
-                                           "<i class='far fa-trash-alt c-red'></i>" +
-                                           "<i class='fas fa-check c-green'></i>" +
+                                           "<i class='fas fa-pencil-alt c-orange edit'></i>" +
+                                           "<i class='far fa-trash-alt c-red delete'></i>" +
+                                           "<i class='fas fa-check c-green save'></i>" +
                                        "</div>" +
                                    "</header>" +
                                    "<textarea placeholder='Nova Nota'></textarea>" +
                                "</div>");
 
            return this.box_single_note;
-
     }
 
-    clickNote(){
-        this.i = 0;
+    clickNote() {
+        if(!this.i) this.i = 1;
+        this.descendentes = this.box_single_note.querySelectorAll(".note");
+        for ( var i = 0; i < this.descendentes.length; i++) {
+            this.descendentes[i].addEventListener("click", function (e) {
+                this.style.display = "none";
+            })
+        }
         console.log(this.box_single_note.querySelectorAll(".note")[this.i]); // return NodeList(div.note)
-         this.i++;
-         console.log(this.i)
     }
-
-
 }
